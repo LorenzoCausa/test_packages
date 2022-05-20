787d98d97b53d784480b857357c29794c201fe92
LorenzoCausa <lorenzo.causa1998@gmail.com>
2022-05-20 10:40:49 +0200

    robine

--- test_pkg/scripts/detectron2_ros.py
+++ test_pkg/scripts/detectron2_ros.py
@@ -184,8 +184,8 @@ def main():
     cfg = get_cfg()
     # add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
     cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.9  # set threshold for this model
-    cfg.MODEL.DEVICE='cpu' # Run on cpu/cuda
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7  # set threshold for this model
+    cfg.MODEL.DEVICE='cuda' # Run on cpu/cuda
     # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
     if(args.weights=="default"):
         cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
