807a18a8bb51038662eea1b48c41f3ec383c4034
LorenzoCausa <lorenzo.causa1998@gmail.com>
2022-05-26 11:42:04 +0200

    barometer

--- test_pkg/scripts/UDP_server.py
+++ test_pkg/scripts/UDP_server.py
@@ -12,20 +12,25 @@ import struct
 FORMAT = "utf-8"      
 
 def sub_server(localIP,localPort):
-    global altitude
     # Create a datagram socket
-    UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    UDPServerSocket1 = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    UDPServerSocket2 = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
     # Bind to address and ip
-    UDPServerSocket.bind((localIP,localPort))
-    print("UDP server up and listening")
+    UDPServerSocket1.bind((localIP,localPort))
+    UDPServerSocket2.bind((localIP,localPort+1))
+    print("UDP servers up and listening")
     # Listen for incoming datagrams
-    count=0
     while not rospy.is_shutdown():
-        bytesAddressPair = UDPServerSocket.recvfrom(8192)
-        message = bytesAddressPair[0]
-        [altitude] = struct.unpack('>f',message)
+        bytesAddressPair1 = UDPServerSocket1.recvfrom(8192)
+        message1 = bytesAddressPair1[0]
+        bytesAddressPair2 = UDPServerSocket2.recvfrom(8192)
+        message2 = bytesAddressPair2[0]
+        [altitude] = struct.unpack('>f',message1)
+        [barometer_altitude] = struct.unpack('>f',message2)
         altitude_pub.publish(altitude)
+        barometer_altitude_pub.publish(barometer_altitude)
         print("ground distance: " , altitude)
+        print("altitude from take off point: ",barometer_altitude)
 
 if __name__ == "__main__":
     '''
@@ -38,6 +43,7 @@ if __name__ == "__main__":
     ip_add = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
     print("IP Address: ", ip_add)
     altitude_pub = rospy.Publisher("/ground_distance", Float32, queue_size=1)
+    barometer_altitude_pub = rospy.Publisher("/barometer_altitude", Float32, queue_size=1)
     
     #creating the server at the specified ip and port
     sub_server(ip_add,8081)
